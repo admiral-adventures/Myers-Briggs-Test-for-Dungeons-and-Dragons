2cda90d69308eac886f66b5988c72d1696bfa231
admiral-adventures <156221739+admiral-adventures@users.noreply.github.com>
2024-04-20 14:16:02 -0400

    Update test-result.tsx

--- components/test/test-result.tsx
+++ components/test/test-result.tsx
@@ -336,13 +336,6 @@ export default function TestResult(props: TestResultProps) {
             {`${explanationOfProblem}`}
           </Text>
         ))}
-        <Button
-        mt={4} // Margin top for spacing
-        colorScheme="blue" // Use Chakra UI color scheme for light blue
-        onClick={() => window.open("https://admiraladventures.games", "_blank")}
-      >
-        Visit Our Website
-      </Button>
       <Heading
         scrollMarginTop={8}
         id="solutions"
@@ -365,13 +358,6 @@ export default function TestResult(props: TestResultProps) {
             {`${solution}`}
           </Text>
         ))}
-        <Button
-        mt={4} // Margin top for spacing
-        colorScheme="blue" // Use Chakra UI color scheme for light blue
-        onClick={() => window.open("https://startplaying.games/gm/admiral-adventures", "_blank")}
-      >
-        Sign Up Here
-      </Button>
       <Heading
         scrollMarginTop={8}
         id="living-happily-tips"
@@ -394,13 +380,6 @@ export default function TestResult(props: TestResultProps) {
             {`${tips}`}
           </Text>
         ))}
-       <Button
-        mt={4} // Margin top for spacing
-        colorScheme="blue" // Use Chakra UI color scheme for light blue
-        onClick={() => window.open("https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile", "_blank")}
-      >
-        LinkedIn Profile
-      </Button>
       {personalityClassGroup.suggestions !== undefined &&
         personalityClassGroup.suggestions.length === 1 && (
           <>
@@ -477,13 +456,6 @@ export default function TestResult(props: TestResultProps) {
           </ListItem>
         ))}
       </UnorderedList>
-        <Button
-        mt={4} // Margin top for spacing
-        colorScheme="blue" // Use Chakra UI color scheme for light blue
-        onClick={() => window.open("https://buymeacoffee.com/admiraladventures", "_blank")}
-      >
-        Sign Up Here
-      </Button>
     </Flex>
   );
 }
