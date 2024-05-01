0de0dee4cbd69516fc3121340d79f3f9197c4b78
admiral-adventures <156221739+admiral-adventures@users.noreply.github.com>
2024-05-01 08:12:30 -0400

    Update personality-class-groups.ts

--- data/personality-class-groups.ts
+++ data/personality-class-groups.ts
@@ -5,7 +5,7 @@ export const personalityClassGroup: PersonalityClassGroup[] = [
     type: "ENFJ",
     name: "The Protagonist",
     nameDescription: "“When the whole world is silent, even one voice becomes powerful.” - Malala Yousafzai",
-    epithet: "Extraverted iNtuitive Feeling Judging",
+    epithet: "Extroverted iNtuitive Feeling Judging",
     description: `Protagonists (ENFJs) feel called to serve a greater purpose in life. Thoughtful and idealistic, these personality types strive to have a positive impact on other people and the world around them. They rarely shy away from an opportunity to do the right thing, even when doing so is far from easy.
 Protagonists are born leaders, which explains why these personalities can be found among many notable politicians, coaches, and teachers. Their passion and charisma allow them to inspire others not just in their careers but in every arena of their lives, including their relationships. Few things bring Protagonists a deeper sense of joy and fulfillment than guiding friends and loved ones to grow into their best selves.`,
     jungianFunctionalPreference: {
@@ -62,7 +62,7 @@ Protagonists are born leaders, which explains why these personalities can be fou
     type: "ENTJ",
     name: "The Commander",
     nameDescription: "“Your time is limited, so don’t waste it living someone else’s life.” - Steve Jobs",
-    epithet: "Extraverted iNtuitive Thinking Judging",
+    epithet: "Extroverted iNtuitive Thinking Judging",
     description: `Commanders are natural-born leaders. People with this personality type embody the gifts of charisma and confidence, and project authority in a way that draws crowds together behind a common goal. However, Commanders are also characterized by an often ruthless level of rationality, using their drive, determination and sharp minds to achieve whatever end they’ve set for themselves. Perhaps it is best that they make up only three percent of the population, lest they overwhelm the more timid and sensitive personality types that make up much of the rest of the world – but we have Commanders to thank for many of the businesses and institutions we take for granted every day.`,
     jungianFunctionalPreference: {
       dominant: "Half-Elf SORCERER - Draconic",
@@ -119,7 +119,7 @@ Protagonists are born leaders, which explains why these personalities can be fou
     type: "ENFP",
     name: "The Campaigner",
     nameDescription: "“It doesn’t interest me what you do for a living. I want to know what you ache for – and if you dare to dream of meeting your heart’s longing.” - Oriah Mountain Dreamer",
-    epithet: "Extraverted iNtuitive Feeling Perceiving",
+    epithet: "Extroverted iNtuitive Feeling Perceiving",
     description: `Campaigners (ENFPs) are true free spirits – outgoing, openhearted, and open-minded. With their lively, upbeat approach to life, they stand out in any crowd. But even though they can be the life of the party, Campaigners don’t just care about having a good time. These personality types run deep – as does their longing for meaningful, emotional connections with other people.`,
     jungianFunctionalPreference: {
       dominant: "Eladrin Spring Elf BARD - College of Glamour",
@@ -176,7 +176,7 @@ Protagonists are born leaders, which explains why these personalities can be fou
     type: "ENTP",
     name: "The Debater",
     nameDescription: "“Follow the path of the unsafe, independent thinker. Expose your ideas to the dangers of controversy.” - Thomas J. Watson",
-    epithet: "Extraverted iNtuitive Thinking Perceiving",
+    epithet: "Extroverted iNtuitive Thinking Perceiving",
     description: `Quick-witted and audacious, Debaters aren’t afraid to disagree with the status quo. In fact, they’re not afraid to disagree with pretty much anything or anyone. Few things light up people with this personality type more than a bit of verbal sparring – and if the conversation veers into controversial terrain, so much the better.
 It would be a mistake, though, to think of Debaters as disagreeable or mean-spirited. Instead, people with this personality type are knowledgeable and curious, with a playful sense of humor, and they can be incredibly entertaining. They simply have an offbeat, contrarian idea of fun – one that involves a healthy dose of spirited debate.`,
     jungianFunctionalPreference: {
@@ -234,7 +234,7 @@ It would be a mistake, though, to think of Debaters as disagreeable or mean-spir
     type: "ESFJ",
     name: "The Consul",
     nameDescription: "“Encourage, lift, and strengthen one another. For the positive energy spread to one will be felt by us all.” - Deborah Day",
-    epithet: "Extraverted Sensing Feeling Judging",
+    epithet: "Extroverted Sensing Feeling Judging",
     description: `For Consuls, life is sweetest when it’s shared with others. People with this personality type form the bedrock of many communities, opening their homes – and their hearts – to friends, loved ones, and neighbors.
 This doesn’t mean that Consuls like everyone, or that they’re saints. But Consuls do believe in the power of hospitality and good manners, and they tend to feel a sense of duty to those around them. Generous and reliable, people with this personality type often take it upon themselves – in ways both large and small – to hold their families and their communities together.`,
     jungianFunctionalPreference: {
@@ -291,7 +291,7 @@ This doesn’t mean that Consuls like everyone, or that they’re saints. But Co
     type: "ESFP",
     name: "The Entertainer",
     nameDescription: "“Live for each second without hesitation.” - Elton John",
-    epithet: "Extraverted Sensing Feeling Perceiving",
+    epithet: "Extroverted Sensing Feeling Perceiving",
     description: `If anyone is to be found spontaneously breaking into song and dance, it is the Entertainer personality type. Entertainers get caught up in the excitement of the moment, and want everyone else to feel that way, too. No other personality type is as generous with their time and energy as Entertainers when it comes to encouraging others, and no other personality type does it with such irresistible style.`,
     jungianFunctionalPreference: {
       dominant: "Human ROGUE - Arcane Trickster",
@@ -346,7 +346,7 @@ This doesn’t mean that Consuls like everyone, or that they’re saints. But Co
     type: "ESTJ",
     name: "The Executive",
     nameDescription: "“Good order is the foundation of all things.” - Edmund Burke",
-    epithet: "Extraverted Sensing Thinking Judging",
+    epithet: "Extroverted Sensing Thinking Judging",
     description: `Executives are representatives of tradition and order, utilizing their understanding of what is right, wrong and socially acceptable to bring families and communities together. Embracing the values of honesty, dedication and dignity, people with the Executive personality type are valued for their clear advice and guidance, and they happily lead the way on difficult paths. Taking pride in bringing people together, Executives often take on roles as community organizers, working hard to bring everyone together in celebration of cherished local events, or in defense of the traditional values that hold families and communities together.`,
     jungianFunctionalPreference: {
       dominant: "Human PALADIN - Oath of Devotion",
@@ -404,7 +404,7 @@ This doesn’t mean that Consuls like everyone, or that they’re saints. But Co
     type: "ESTP",
     name: "The Entrepreneur",
     nameDescription: "“Life is either a daring adventure or nothing at all.” - Helen Keller",
-    epithet: "Extraverted Sensing Thinking Perceiving",
+    epithet: "Extroverted Sensing Thinking Perceiving",
     description: `Entrepreneurs always have an impact on their immediate surroundings – the best way to spot them at a party is to look for the whirling eddy of people flitting about them as they move from group to group. Laughing and entertaining with a blunt and earthy humor, Entrepreneur personalities love to be the center of attention. If an audience member is asked to come on stage, Entrepreneurs volunteer – or volunteer a shy friend.
 Theory, abstract concepts and plodding discussions about global issues and their implications don’t keep Entrepreneurs interested for long. Entrepreneurs keep their conversation energetic, with a good dose of intelligence, but they like to talk about what is – or better yet, to just go out and do it. Entrepreneurs leap before they look, fixing their mistakes as they go, rather than sitting idle, preparing contingencies and escape clauses.`,
     jungianFunctionalPreference: {
