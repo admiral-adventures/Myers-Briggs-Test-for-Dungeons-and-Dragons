cc1d0193700f6f6f5055a4808fc5524cd1371843
admiral-adventures <156221739+admiral-adventures@users.noreply.github.com>
2024-05-24 14:50:00 -0400

    Update test-result-stats.tsx

--- components/test/test-result-stats.tsx
+++ components/test/test-result-stats.tsx
@@ -1,5 +1,4 @@
 import { Flex, Text, Heading } from "@chakra-ui/react";
-
 import { personalityClasses } from "../../data/personality-classes";
 import { PersonalityClass, TestResult } from "../../lib/personality-test";
 
@@ -10,67 +9,39 @@ interface TestResultStatsProps {
 function ScoreStats(props: {
   testScores: PersonalityClass["type"][];
   targetScore: PersonalityClass["type"];
+  totalQuestions: number;
 }) {
-  const testScoresFiltered = props.testScores.filter(
+  const totalForPair = props.testScores.filter(
     (score) => score === props.targetScore
-  );
+  ).length;
+  const percentage = ((totalForPair / props.totalQuestions) * 100).toFixed(0);
 
   return (
-    <Flex
-      py={1}
-      px={2}
-      gap={2}
-      rounded="md"
-      justifyContent="space-between"
-      bg="white"
-    >
-      <Text fontWeight="semibold">
-        {((testScoresFiltered.length / props.testScores.length) * 100)
-          .toFixed(2)
-          .replace(/[.,]0+$/, "")}
-        %
-      </Text>
-      <Text>({testScoresFiltered.length})</Text>
+    <Flex py={1} px={2} gap={2} rounded="md" justifyContent="space-between" bg="white">
+      <Text fontWeight="semibold">{percentage}%</Text>
+      <Text>({totalForPair})</Text>
     </Flex>
   );
 }
 
 export default function TestResultStats(props: TestResultStatsProps) {
-  const statsColorScheme = [
-    "red",
-    "blue",
-    "yellow",
-    "purple",
-    "orange",
-    "green",
-    "pink",
-    "teal",
-  ];
+  const totalQuestions = 70; // Total number of questions in the MBTI quiz
+  const statsColorScheme = ["red", "blue", "yellow", "purple", "orange", "green", "pink", "teal"];
 
   return (
     <Flex
       my={4}
       mx={{ base: 0, lg: 4 }}
-      w={{
-        base: "full",
-        lg: "25%",
-      }}
+      w={{ base: "full", lg: "25%" }}
       h="min-content"
       p={2}
       gap={4}
       top={5}
       direction="column"
-      pos={{
-        base: "static",
-        lg: "sticky",
-      }}
+      pos={{ base: "static", lg: "sticky" }}
       alignSelf="flex-start"
     >
-      <Heading
-        as="h1"
-        textAlign="center"
-        fontSize="lg"
-      >
+      <Heading as="h1" textAlign="center" fontSize="lg">
         Scores
       </Heading>
       {personalityClasses.map((personalityClass, index) => (
@@ -82,15 +53,13 @@ export default function TestResultStats(props: TestResultStatsProps) {
           alignItems="center"
           bg={`${statsColorScheme[index]}.500`}
         >
-          <Text
-            fontWeight="semibold"
-            color="white"
-          >
+          <Text fontWeight="semibold" color="white">
             {personalityClass.description}
           </Text>
           <ScoreStats
             testScores={props.testResult.testScores}
             targetScore={personalityClass.type}
+            totalQuestions={totalQuestions}
           />
         </Flex>
       ))}
