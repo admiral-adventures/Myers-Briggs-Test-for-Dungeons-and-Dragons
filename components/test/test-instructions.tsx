c467aff99c1b7b1c34e60c6abd5e7d0a61460555
admiral-adventures <156221739+admiral-adventures@users.noreply.github.com>
2026-04-14 12:46:18 -0400

    Update test-instructions.tsx

--- components/test/test-instructions.tsx
+++ components/test/test-instructions.tsx
@@ -21,13 +21,29 @@ export default function TestInstructions(props: TestInstructionsProps) {
     >
       <Heading>Guidelines</Heading>
 
+        <Text>
+          This test is designed as a {" "}
+            <Text as="span" fontWeight="semibold">
+              36-scene D&D adventure campaign
+            </Text>{" "}, following your party from low-level goblin 
+          raids to legendary battles against dragons, liches, and the great villains of Faerûn.
+
+          To get the most accurate result, approach each question as if you are {" "}
+            <Text as="span" fontWeight="semibold">
+            inside the scene in that exact moment.
+            </Text>{" "} 
+        </Text>
+      
       <Flex
         direction="column"
         gap={2}
       >
         <UnorderedList spacing={2}>
           <ListItem>
-            Answer as if you were an actual D&D character in a typical game scenario.
+           {" "}
+            <Text as="span" fontWeight="semibold">
+            Answer by instinct.
+            </Text>{" "} . Choose the response that feels most natural in the moment, even if it is not the “best” tactical choice.
           </ListItem>
           <ListItem>
             Respond honestly — choose what feels natural to you,{" "}
