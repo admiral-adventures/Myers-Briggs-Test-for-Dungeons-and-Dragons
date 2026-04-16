eaf8689d39045069d2c788e3f73749079cba8557
admiral-adventures <156221739+admiral-adventures@users.noreply.github.com>
2026-04-16 12:38:53 -0400

    Update test-result.tsx

--- components/test/test-result.tsx
+++ components/test/test-result.tsx
@@ -261,7 +261,7 @@ return (
         >
           <Stack spacing={3}>
       
-            <Text fontWeight="bold" fontSize="lg" textAlign="center">
+            <Text fontWeight="bold" fontSize="xl" textAlign="center">
               FIGHTER OPTION
             </Text>
       
@@ -306,7 +306,7 @@ return (
         >
           <Stack spacing={3}>
       
-            <Text fontWeight="bold" fontSize="lg" textAlign="center">
+            <Text fontWeight="bold" fontSize="xl" textAlign="center">
               CLERIC OPTION
             </Text>
       
@@ -351,7 +351,7 @@ return (
         >
           <Stack spacing={3}>
       
-            <Text fontWeight="bold" fontSize="lg" textAlign="center">
+            <Text fontWeight="bold" fontSize="xl" textAlign="center">
               ROGUE OPTION
             </Text>
       
@@ -396,7 +396,7 @@ return (
         >
           <Stack spacing={3}>
       
-            <Text fontWeight="bold" fontSize="lg" textAlign="center">
+            <Text fontWeight="bold" fontSize="xl" textAlign="center">
               WIZARD OPTION
             </Text>
       
