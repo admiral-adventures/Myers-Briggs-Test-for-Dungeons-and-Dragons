b6964de9bd344456d99f88c2b1f32420dd6c1802
admiral-adventures <156221739+admiral-adventures@users.noreply.github.com>
2026-05-04 16:36:05 -0400

    Update submit-results.ts

--- pages/api/submit-results.ts
+++ pages/api/submit-results.ts
@@ -1,6 +1,10 @@
 import { Client } from "pg";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
